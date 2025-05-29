88e505c769369886efb9360a470739b2bba909b6
Seungho Baek <sbsh.baek@samsung.com>
2025-05-29 21:07:02 +0900

    [TEST] Ignored Test
    Signed-off-by: Seungho Baek <sbsh.baek@samsung.com>

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CaptureTest.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CaptureTest.cs
@@ -9,109 +9,107 @@ namespace Tizen.NUI.Samples
     using log = Tizen.Log;
     public class CaptureTest : IExample
     {
+        View viewB;
+
+        bool isGenB = false;
+
+        int cellCount = 50;
+
+        Animation animation;
+
         public void Activate()
         {
-            log.Debug(tag, $"Activate(): start \n");
-            resourcePath = Tizen.Applications.Application.Current.DirectoryInfo.Resource;
-
             window = NUIApplication.GetDefaultWindow();
             window.TouchEvent += Win_TouchEvent;
 
-            root = new View()
+            Vector2 windowHalfSize = new Vector2(window.Size.Width/2.0f, window.Size.Height);
+
+            View viewA = new View()
             {
-                Name = "test_root",
-                Size = new Size(500, 500),
-                Position = new Position(10, 10),
-                BackgroundColor = Color.White,
+                PositionUsesPivotPoint = true,
+                Size = new Size(windowHalfSize.X, windowHalfSize.Y),
+                PivotPoint = PivotPoint.TopLeft,
+                ParentOrigin = ParentOrigin.TopLeft
             };
+            window.Add(viewA);
 
-            window.Add(root);
-
-            log.Debug(tag, $"root view added \n");
-
-            capturedView0 = new ImageView(resourcePath + "/images/image1.jpg")
+            animation = new Animation(5000)
             {
-                Name = "test_v0",
-                Size = new Size(100, 100),
-                BackgroundColor = Color.Red,
+                Looping = true,
             };
-            root.Add(capturedView0);
+            animation.AnimateTo(viewA, "PositionX", 100.0f);
+            animation.Play();
 
-            capturedView1 = new ImageView(resourcePath + "/images/image2.jpg")
+            viewB = new View()
             {
-                Name = "test_v1",
-                Size = new Size(150, 150),
-                Position = new Position(150, 150),
-                BackgroundColor = Color.Yellow,
+                PositionUsesPivotPoint = true,
+                Size = new Size(windowHalfSize.X, windowHalfSize.Y),
+                PivotPoint = PivotPoint.TopRight,
+                ParentOrigin = ParentOrigin.TopRight
             };
-            root.Add(capturedView1);
+            window.Add(viewB);
+            viewB.Ignored = true;
 
-            //TDD
-            //tddTest();
-            //checkCaptureNew();
+            Size viewSize = new Size(windowHalfSize.X / cellCount, windowHalfSize.Y / cellCount);
+            for (int i = 0; i < cellCount; ++i)
+            {
+                for (int j = 0; j < cellCount; ++j)
+                {
+                    View view = new View()
+                    {
+                        BackgroundColor = Color.Red,
+                        BorderlineWidth = 3.0f,
+                        PivotPoint = PivotPoint.TopLeft,
+                        ParentOrigin = ParentOrigin.TopLeft,
+                        Position = new Position(viewSize.Width * i, viewSize.Height * j),
+                        Size = viewSize
+                    };
+                    viewA.Add(view);
+                }
+            }
         }
 
-        private void onCaptureFinished(object sender, CaptureFinishedEventArgs e)
+        private void GenB()
         {
-            log.Debug(tag, $"onCaptureFinished() statue={e.Success} \n");
-
-            if (sender is Capture)
+            if(isGenB)
             {
-                log.Debug(tag, $"sender is Capture \n");
-                ImageUrl imageUrl = capture.GetImageUrl();
-                capturedImage = new ImageView(imageUrl.ToString());
-                log.Debug(tag, $"url={imageUrl.ToString()} \n");
-
-                capturedImage.Size = new Size(510, 510);
-                capturedImage.Position = new Position(10, 10);
-                root.Add(capturedImage);
-                done = false;
+                return;
             }
+
+            Vector2 windowHalfSize = new Vector2(window.Size.Width/2.0f, window.Size.Height);
+            Size viewSize = new Size(windowHalfSize.X / cellCount, windowHalfSize.Y / cellCount);
+            for (int i = 0; i < cellCount; ++i)
+            {
+                for (int j = 0; j < cellCount; ++j)
+                {
+                    View view = new View()
+                    {
+                        BackgroundColor = Color.Blue,
+                        BorderlineWidth = 3.0f,
+                        PivotPoint = PivotPoint.TopLeft,
+                        ParentOrigin = ParentOrigin.TopLeft,
+                        Position = new Position(viewSize.Width * i, viewSize.Height * j),
+                        Size = viewSize
+                    };
+                    viewB.Add(view);
+                }
+            }
+            isGenB = true;
         }
 
         private void Win_TouchEvent(object sender, Window.TouchEventArgs e)
         {
             if (e.Touch.GetState(0) == PointStateType.Down)
             {
-                if (!done)
-                {
-                    done = true;
-                    capture = new Capture();
-                    capture.Start(root, new Size(510, 510), "");
-                    capture.Finished += onCaptureFinished;
-                    log.Debug(tag, $"capture done \n");
-                }
+                GenB();
+                viewB.Ignored = !viewB.Ignored;
             }
-        }
-
-        private void tddTest()
-        {
-            log.Debug(tag, $"TDD test before Assert");
-
-            Assert.IsFalse(true, "TDD test, Exception throw");
-
-            Assert.IsFalse(false, "TDD test, Exception throw");
-
-            log.Debug(tag, $"TDD test after Assert");
-        }
-
-        private void checkCaptureNew()
-        {
-            var target = new Capture();
-            Assert.IsNotNull(target, "target should not be null");
-            Assert.IsTrue(target is Capture, "target should be Capture class");
         }
 
         public void Deactivate()
         {
         }
 
-        const string tag = "NUITEST";
         private Window window;
-        private View root, capturedView0, capturedView1;
-        private Capture capture;
-        private ImageView capturedImage;
-        private bool done = false;
-        private string resourcePath;
     }
 }
