b5d79503fe605e3065f46a9bdb03cee26c3d416f
Seungho Baek <sbsh.baek@samsung.com>
2025-06-23 13:48:45 +0900

    [Test] Glassmorphism
    Signed-off-by: Seungho Baek <sbsh.baek@samsung.com>

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CaptureTest.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CaptureTest.cs
@@ -3,115 +3,122 @@ using global::System;
 using Tizen.NUI;
 using Tizen.NUI.BaseComponents;
 using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using Tizen.NUI.Text;
+using Tizen.NUI.BaseComponents.VectorGraphics;
 
 namespace Tizen.NUI.Samples
 {
     using log = Tizen.Log;
     public class CaptureTest : IExample
     {
+        private Window window;
+
         public void Activate()
         {
-            log.Debug(tag, $"Activate(): start \n");
-            resourcePath = Tizen.Applications.Application.Current.DirectoryInfo.Resource;
-
             window = NUIApplication.GetDefaultWindow();
             window.TouchEvent += Win_TouchEvent;
 
-            root = new View()
+            ImageView imageView = new ImageView()
             {
-                Name = "test_root",
-                Size = new Size(500, 500),
-                Position = new Position(10, 10),
-                BackgroundColor = Color.White,
+                ResourceUrl = "/home/seungho/Shared/test2.jpg",
+                WidthResizePolicy = ResizePolicyType.FillToParent,
+                HeightResizePolicy = ResizePolicyType.FillToParent,                
             };
+            window.Add(imageView);
 
-            window.Add(root);
+            View cardView = CreateCardViewStyle(new Size(500.0f, 300.0f), 20.0f);
+            window.Add(cardView);
 
-            log.Debug(tag, $"root view added \n");
-
-            capturedView0 = new ImageView(resourcePath + "/images/image1.jpg")
+            TextLabel name = new TextLabel("Tizen Platform Lab")
             {
-                Name = "test_v0",
-                Size = new Size(100, 100),
-                BackgroundColor = Color.Red,
+                PointSize = 9,
+                TextColor = new Vector4(0.7f, 0.7f, 0.7f, 1.0f),
+                PositionUsesPivotPoint = true,
+                PivotPoint = PivotPoint.TopLeft,
+                ParentOrigin = ParentOrigin.TopLeft,
+                Position = new Position(30.0f, 120.0f)
             };
-            root.Add(capturedView0);
+            cardView.Add(name);
 
-            capturedView1 = new ImageView(resourcePath + "/images/image2.jpg")
+            TextLabel number = new TextLabel("1 2 3 4    5 6 7 8   9 1 0 1   1 1 2 1")
             {
-                Name = "test_v1",
-                Size = new Size(150, 150),
-                Position = new Position(150, 150),
-                BackgroundColor = Color.Yellow,
+                PointSize = 12,
+                TextColor = new Vector4(0.7f, 0.7f, 0.7f, 1.0f),
+                PositionUsesPivotPoint = true,
+                PivotPoint = PivotPoint.TopLeft,
+                ParentOrigin = ParentOrigin.TopLeft,
+                Position = new Position(30.0f, 150.0f)
             };
-            root.Add(capturedView1);
+            cardView.Add(number);
 
-            //TDD
-            //tddTest();
-            //checkCaptureNew();
+            TextLabel date = new TextLabel("10/30")
+            {
+                PointSize = 10,
+                TextColor = new Vector4(0.7f, 0.7f, 0.7f, 1.0f),
+                PositionUsesPivotPoint = true,
+                PivotPoint = PivotPoint.TopLeft,
+                ParentOrigin = ParentOrigin.TopLeft,
+                Position = new Position(30.0f, 250.0f)
+            };
+            cardView.Add(date);
         }
 
-        private void onCaptureFinished(object sender, CaptureFinishedEventArgs e)
+        private View CreateCardViewStyle(Size size, float cornerRadius)
         {
-            log.Debug(tag, $"onCaptureFinished() statue={e.Success} \n");
-
-            if (sender is Capture)
+            float borderlineWidth = 3.0f;
+            Visuals.ColorVisual borderLine = new Visuals.ColorVisual()
             {
-                log.Debug(tag, $"sender is Capture \n");
-                ImageUrl imageUrl = capture.GetImageUrl();
-                capturedImage = new ImageView(imageUrl.ToString());
-                log.Debug(tag, $"url={imageUrl.ToString()} \n");
+                Color = Color.Transparent,
+                BorderlineColor = new Color(0.7f, 0.7f, 0.7f, 0.5f),
+                BorderlineOffset = 1.0f,
+                BorderlineWidth = borderlineWidth,
+                CornerRadius = cornerRadius + borderlineWidth,
+                OffsetXPolicy = VisualTransformPolicyType.Absolute,
+                OffsetYPolicy = VisualTransformPolicyType.Absolute,
+                CutoutPolicy = ColorVisualCutoutPolicyType.CutoutViewWithCornerRadius,
+            };
 
-                capturedImage.Size = new Size(510, 510);
-                capturedImage.Position = new Position(10, 10);
-                root.Add(capturedImage);
-                done = false;
-            }
+            Visuals.ColorVisual shadowVisual = new Visuals.ColorVisual()
+            {
+                Name = "shadow2",
+                Color = new Vector4 (0.3f, 0.3f, 0.3f, 1.0f),
+                BlurRadius = 100.0f,
+                CornerRadius = cornerRadius,
+                OffsetXPolicy = VisualTransformPolicyType.Absolute,
+                OffsetYPolicy = VisualTransformPolicyType.Absolute,
+                CutoutPolicy = ColorVisualCutoutPolicyType.CutoutViewWithCornerRadius,
+            };
+
+            View card = new View()
+            {
+                Name = "test_root",
+                Size = size,
+                CornerRadius = cornerRadius,
+                PositionUsesPivotPoint = true,
+                PivotPoint = PivotPoint.Center,
+                ParentOrigin = ParentOrigin.Center,
+            };
+            card.SetRenderEffect(RenderEffect.CreateBackgroundBlurEffect(150.0f));
+            card.AddVisual(borderLine);
+            card.AddVisual(shadowVisual);
+            borderLine.LowerToBottom();
+            shadowVisual.LowerToBottom();
+
+            return card;
         }
 
         private void Win_TouchEvent(object sender, Window.TouchEventArgs e)
         {
             if (e.Touch.GetState(0) == PointStateType.Down)
             {
-                if (!done)
-                {
-                    done = true;
-                    capture = new Capture();
-                    capture.Start(root, new Size(510, 510), "");
-                    capture.Finished += onCaptureFinished;
-                    log.Debug(tag, $"capture done \n");
-                }
             }
-        }
-
-        private void tddTest()
-        {
-            log.Debug(tag, $"TDD test before Assert");
-
-            Assert.IsFalse(true, "TDD test, Exception throw");
-
-            Assert.IsFalse(false, "TDD test, Exception throw");
-
-            log.Debug(tag, $"TDD test after Assert");
-        }
-
-        private void checkCaptureNew()
-        {
-            var target = new Capture();
-            Assert.IsNotNull(target, "target should not be null");
-            Assert.IsTrue(target is Capture, "target should be Capture class");
         }
 
         public void Deactivate()
         {
         }
-
-        const string tag = "NUITEST";
-        private Window window;
-        private View root, capturedView0, capturedView1;
-        private Capture capture;
-        private ImageView capturedImage;
-        private bool done = false;
-        private string resourcePath;
     }
 }
